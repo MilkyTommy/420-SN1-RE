fa3eb676df7dc09b5954a2ad7fcb94ea0b3adfe7
Tommy Gagnon Joyal <tommygjoyal@gmail.com>
2024-02-15 06:30:06 -0500

    script v1

--- quiz1/corrector.py
+++ quiz1/corrector.py
@@ -1,71 +1,109 @@
 import argparse
 import pandas as pd
 import warnings
-import glob, os
+import glob, os, sys
 
-warnings.filterwarnings('ignore')
+DEFAULT_SCORE = 15
+PATH_OF_SCRIPT = sys.path[0]
 
-SCORE = 15
+warnings.filterwarnings("ignore", "\nPyarrow", DeprecationWarning)
+
+def getAnwsersFromAFile(filename, isHeaderRequired = False) -> list[str]:
+    df = pd.read_excel(filename,sheet_name=1)
+    anwsers = []
+    
+    for i in range(5,5+max_score) :
+        anwsers.append(df.iloc[i, 2])
+
+    if isHeaderRequired :
+        return {'firstname' : df.iloc[1,1],
+                'lastname' : df.iloc[1,2],
+                'id' : df.iloc[1,3],
+                'group' : df.iloc[1,4],
+                'score' : anwsers
+                }
+
+    return {'score' : anwsers}
+
+def getSingleScore(input_filename, output_filename)-> dict:
+    total = 0
+
+    anwsers = getAnwsersFromAFile(input_filename, True)
+    correctedAnwsers = getAnwsersFromAFile(output_filename)
+
+    for i in range(0,max_score) :
+        total += int(anwsers['score'][i] == correctedAnwsers['score'][i])
+    
+    anwsers['score'] = total
+    return anwsers
+
+def writeAnwsersToFile(data, filename) -> str :
+    # Create a DataFrame
+    df = pd.DataFrame(data)
+
+    # Export the DataFrame to an Excel file
+    df.to_excel(filename + '.xlsx', index=False)
+
+    return PATH_OF_SCRIPT + filename + '.xlsx'
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-s","--score", help="Choos max score",type=int)
+    parser.add_argument("type", help="Choose between solo or all files",type=str, choices=["single","all"])
     parser.add_argument("-i", "--input", help="select a input file")
     parser.add_argument("-o", "--output", help="select a output file")
     args = parser.parse_args()
 
-    if args.input and args.output:
-        df_in = pd.read_excel(args.input,sheet_name=1)
-
-        firstname = df_in.iloc[1][1]
-        lastname = df_in.iloc[1][2]
-        id = df_in.iloc[1][3]
-        group = df_in.iloc[1][4]
-
-        anwsers = []
-        for i in range(5,5+SCORE) :
-            anwsers.append(df_in.iloc[i][2])
-
-        df_out = pd.read_excel(args.output,sheet_name=1)
-        correct_anwsers = []
-        
-        for i in range(5,5+SCORE) :
-            correct_anwsers.append(df_out.iloc[i][2])
-        
-        total = 0
-        for i in range(SCORE) :
-            total += int(correct_anwsers[i] == anwsers[i])
-
-        print("nom :",firstname,lastname, ",id :", id, ",groupe :", group)
-        print(total,"/",SCORE)
+    if args.score :
+        max_score = args.score
     else :
-        filename = os.chdir("./quiz1/solution/")
+        max_score = DEFAULT_SCORE
+
+    if args.type == "single" :
+        if args.input and args.output:
+            student = getSingleScore(args.input, args.output)
+
+            print("Nom :",student['firstname'],student['lastname']) 
+            print("DA :", student['id'])
+            print("Groupe :", student['group'])
+            print("Score :",student['score'],"/",max_score)
+        else :
+            parser.error("single requires --input and --output.")         
+    elif args.type =="all" :
+        filename = os.chdir(PATH_OF_SCRIPT + "/solution/")
         correct_anwsers = []
-        print(os.getcwd())
 
         for filename in glob.glob("*.xlsx"):
-            df_solution = pd.read_excel(filename,sheet_name=1)
-            for i in range(5,5+SCORE) :
-                correct_anwsers.append(df_solution.iloc[i][2])
+            correct_anwsers =getAnwsersFromAFile(filename)
         
-        filename = os.chdir("../quiz1/etudiant/")
-        # for filename in glob.glob("*.xlsx"):
-        #     df_student = pd.read_excel(filename,sheet_name=1)
-        #     print(df_student)
-        #     firstname = df_student.iloc[1][1]
-        #     lastname = df_student.iloc[1][2]
-        #     id = df_student.iloc[1][3]
-        #     group = df_student.iloc[1][4]
+        filename = os.chdir(PATH_OF_SCRIPT + "/etudiant/")
+        nom = []
+        id = []
+        groupe = []
+        anwsersOfSingleStudent = []
+        totalOfSingleStudent = []
 
-        #     anwsers = []
-        #     for i in range(5,5+SCORE) :
-        #         anwsers.append(df_student.iloc[i][2])
+        for filename in glob.glob("*.xlsx"):
+            anwsers =getAnwsersFromAFile(filename,True)
 
-        # total = 0
-        # for i in range(SCORE) :
-        #     total += int(correct_anwsers[i] == anwsers[i])
-        
-        # print("nom :",firstname,lastname, ",id :", id, ",groupe :", group)
-        # print(total,"/",SCORE)
+            nom.append(anwsers['firstname'] + ' ' + anwsers['lastname'])
+            id.append(anwsers['id'])
+            groupe.append(anwsers['group'])
+            anwsersOfSingleStudent.append(' '.join(anwsers['score']))
+            total = 0
 
+            for i in range(0, max_score) :
+                total += int(anwsers['score'][i] == correct_anwsers['score'][i])
+
+            totalOfSingleStudent.append(total)
             
+
+        finalOutput = {
+            'Nom' : nom,
+            'DA' : id,
+            'Groupe' : groupe,
+            'Réponses' : anwsersOfSingleStudent,
+            'Score Total' : totalOfSingleStudent
+        }
+        print(writeAnwsersToFile(finalOutput,'AllStudent'))
